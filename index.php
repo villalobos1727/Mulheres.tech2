99d59f829f6ca6dfae6c8ac07cbfc0bc4f64e717
villalobos1727 <villalobosyeximar27@gmail.com>
2022-10-17 16:52:49 -0300

    17.10

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+
+$mesangem = "Estou com fomen";
+
+$titulo = 'meu site Yeximar';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>meu site Yeximar</title>
+</head>
+<body>
+  
+<?php
+
+echo "Ola mundo!";
+
+
+echo '<br> Estou aqui!';
+
+echo '<p> E voce?';
+
+echo $mesangem;
+
+echo '<br> 18';
+
+?>
+
+</body>
+</html>
